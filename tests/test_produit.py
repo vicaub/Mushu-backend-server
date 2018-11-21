0ead6ee418187e154edb5855e743dcb466311213
Victor Aubin <aubin.victor@gmail.com>
2018-11-21 15:42:25 +0100

    added product viandes

--- tests/test_produit.py
+++ tests/test_produit.py
@@ -4,3 +4,24 @@ legume1 = Product("courgette", None)
 legume2 = Product("poivron", 1.3)
 legume3 = Product("oignon", 0, [legume1, legume2])
 
+try:
+    Product(1)
+    raise Exception("TypeError expected")
+except TypeError:
+    pass
+
+try:
+    Product("aubergine", "12")
+    raise Exception("TypeError expected")
+except TypeError:
+    pass
+
+try:
+    Product("aubergine", 12, 3)
+    raise Exception("TypeError expected")
+except TypeError:
+    pass
+
+import categories.viandes as viandes
+viandes.viande
+
