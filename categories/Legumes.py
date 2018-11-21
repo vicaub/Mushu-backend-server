a3716158f5e39b3298504d25872202a06721da3e
CamilleNathanAbou <43875691+CamilleNathanAbou@users.noreply.github.com>
2018-11-21 14:49:42 +0100

    sous category tomate

--- categories/Legumes.py
+++ categories/Legumes.py
@@ -1,11 +1,11 @@
-from Produit import Produit
-legumes = Produit('Legume', None)
+from Product import Product
+legumes = Product('Legume', None)
 
-tomate = Produit('tomate', None,
-                 [Produit('sauce tomate', 2.9),
-                  Produit('tomate fraiche hors saison', 2.2),
-                  Produit("tomate fraîche saison", 0.3),
-                  Produit("tomate destinée à l'industrie", 0.5),
-                  Produit('tomate pelées', 1.4),
-                  Produit('pulpe de tomate', 1.4),
-                  Produit('tomate fraiche importée', 0.6)])
+tomate = Product('tomate', None,
+                 [Product('sauce tomate', 2.9),
+                  Product('tomate fraiche hors saison', 2.2),
+                  Product("tomate fraîche saison", 0.3),
+                  Product("tomate destinée à l'industrie", 0.5),
+                  Product('tomate pelées', 1.4),
+                  Product('pulpe de tomate', 1.4),
+                  Product('tomate fraiche importée', 0.6)])
