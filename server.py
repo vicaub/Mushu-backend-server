cdcd1fd3c9ef549db7d769e74a55a26934538f19
Victor Aubin <aubin.victor@gmail.com>
2019-01-23 13:27:04 +0100

    adding 404 file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 from carbon_footprint_calculator import get_cfp_from_barcode
 
@@ -12,6 +12,11 @@ def get_cfp():
     result = get_cfp_from_barcode(barcode)
     return result
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run()
