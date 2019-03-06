7170ab1bf25f2fade74f4d936716d43d54a3651b
Victor Aubin <aubin.victor@gmail.com>
2019-03-06 15:04:02 +0100

    tests for original category

--- tests/test_produit.py
+++ tests/test_produit.py
@@ -1,7 +1,7 @@
 from models.Product import Product
 import unittest
 
-from categories import produits_laitiers, poissons
+from categories import database
 
 legume1 = Product("courgette", None)
 legume2 = Product("poivron", 1.3)
@@ -33,13 +33,17 @@ var = viandes.viandes
 class TestMatching(unittest.TestCase):
 
     def test_get_cfp(self):
-        fromage_product = produits_laitiers.produits_laitiers.children[0].children[3]
+        fromage_product = database.database.children[3].children[0].children[3]
 
         self.assertEqual(fromage_product.cfp, 5.44)
 
-        truite_product = poissons.poissons.children[1]
+        truite_product = database.database.children[2].children[1]
 
         self.assertEqual(truite_product.cfp, 4.2)
+
+    def test_set_category(self):
+        epicerie_prod1 = database.database.children[5].children[0].children[1]
+        self.assertEqual(epicerie_prod1.category, database.database.children[5])
 
 if __name__ == "__main__":
     unittest.main()
