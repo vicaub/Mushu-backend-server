67e3078ef7908d3097f4d8e54c1fdf1fc3243bd1
Victor Aubin <aubin.victor@gmail.com>
2019-03-28 15:44:27 +0100

    Revert "removing \n from ingredient string"
    This reverts commit 3da2803d72279e93a019bf970ca6d06743defbec.

--- models/Ingredient.py
+++ models/Ingredient.py
@@ -17,10 +17,7 @@ class Ingredient:
         self.percent = percent
         self.children = children
         self.match = None
-        self.ingredient_string = ingredient_string
-        if ingredient_string:
-            self.ingredient_string = self.ingredient_string.replace(r"[\n\r]", "")
-
+        self.ingredient_string = ingredient_string.replace(r"[\n\r]", "")
         self.cfp = None
 
         self.remove_useless_words()
