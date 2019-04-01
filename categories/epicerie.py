c9a05741f7a40465a8e3f1f9121abdd97cc63ed7
Victor Aubin <aubin.victor@gmail.com>
2019-04-01 11:05:49 +0200

    add stuff database

--- categories/epicerie.py
+++ categories/epicerie.py
@@ -34,7 +34,9 @@ epicerie = Product("Épicerie", None, [
         Product("Thym", 2.87),
         Product("Autres épices", 3.19),
         Product("Origan", None),
-        Product("Paprika")
+        Product("Paprika"),
+        Product("arome"),
+        Product("curcuma")
     ]),
     Product("Sauces", None, [
         Product("Ketchup", 2.70),
